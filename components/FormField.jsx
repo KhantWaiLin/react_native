350b4b664da64eb87e16c6e0ea4554900b4a27f0
KhantWaiLin <khantkhant.cm@gmail.com>
2024-05-05 20:41:38 +0700

    finishing 3 screens

---
+++ components/FormField.jsx
@@ -0,0 +1,45 @@
+import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+import React, { useState } from "react";
+
+import { icons } from "../constants";
+
+const FormField = ({
+  title,
+  value,
+  placeholder,
+  handleChangeText,
+  otherStyles,
+  ...props
+}) => {
+  const [showPassword, setShowPassword] = useState(false);
+  return (
+    <View className={`space-y-2 ${otherStyles} `}>
+      <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
+      <View
+        className="border-2 border-black-200 flex-row rounded-2xl focus:border-secondary 
+        w-full h-16 bg-black-100 items-center px-4 "
+      >
+        <TextInput
+          className="flex-1 text-white font-psemibold text-base"
+          value={value}
+          placeholder={placeholder}
+          placeholderTextColor="#7b7b8b"
+          onChangeText={handleChangeText}
+          secureTextEntry={title === "Password" && !showPassword}
+          {...props}
+        />
+        {title === "Password" && (
+          <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+            <Image
+              source={!showPassword ? icons.eye : icons.eyeHide}
+              className="w-6 h-6"
+              resizeMode="contain"
+            />
+          </TouchableOpacity>
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default FormField;
